97c8a596cecc4832e61c2baafa3169afaf266639
Abdo73873 <abdelrahman73873@gmail.com>
2025-07-29 16:54:45 +0400

    finish testing

--- lib/turnable_page.dart
+++ lib/turnable_page.dart
@@ -1,37 +1,3 @@
-// // Event system
-// export 'src/event/event_object.dart';
-
-// // Page system
-// export 'src/page/book_page.dart';
-// export 'src/page/book_page_impl.dart';
-// export 'src/page/page_flip.dart';
-
-// // Render system
-// export 'src/render/render.dart';
-
-// // Collection system
-// export 'src/collection/page_collection.dart';
-// export 'src/collection/page_collection_impl.dart';
-
-// // enums
-// export 'src/enums/animation_process.dart';
-// export 'src/enums/book_orientation.dart';
-// export 'src/enums/flip_corner.dart';
-// export 'src/enums/flip_direction.dart';
-// export 'src/enums//flipping_state.dart';
-// export 'src/enums/page_density.dart';
-// export 'src/enums/page_orientation.dart';
-// export 'src/enums/size_type.dart';
-
-// // models
-// export 'src/model/page_rect.dart';
-// export 'src/model/page_state.dart';
-// export 'src/model/point.dart';
-// export 'src/model/rect.dart';
-// export 'src/model/rect_points.dart';
-// export 'src/model/segment.dart';
-// export 'src/model/shadow.dart';
-// export 'src/model/swipe_data.dart';
 
 // Widget
 export 'src/widgets/page_flip_controller.dart';
